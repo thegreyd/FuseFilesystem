14fc07afac871cb713d928ea99939331a952d7e7
Siddharth Sharma <sid.sharma0@gmail.com>
2016-11-17 00:34:43 -0500

    introduce major structural changes. stable

--- hello.c
+++ hello.c
@@ -18,129 +18,117 @@
 #include <stdlib.h>
 #include "rdstructs.h"
 
-filesystem f;
 FILE *fp;
 char *filename = "ramdisk.save";
 
-int print_contents()
+filesystem f;
+
+void nodes_init()
 {
-	printf("---Contents of file system---\n");
-	for(int i = 0; i < f.nnodes; i++) {
-		if ( f.node_bitmap[i] == 1 ) {
-			printf("%s\n", f.nodes[i].path);
-		}
+	printf("----------nodes init\n");
+	for (int i = 0; i < f.nnodes; i++) {
+		f.nodes[i].status = unused;
+		f.nodes[i].size = 0;
+		f.nodes[i].start_block = -1;
+		f.nodes[i].is_dir = 0;
 	}
-	return 0;
 }
 
-int get_free_bit(int *bitmap, int size)
+void blocks_init()
 {
-	for (int i = 0; i < size; i++) {
-		if ( bitmap[i] == 0 )
-			return i;
+	printf("----------blocks init\n");
+	for (int i = 0; i < f.nblocks; i++) {
+		f.blocks[i].status = unusedblock;
+		f.blocks[i].next_block = -1;
 	}
-	return -1;
+}
+
+int print_info()
+{
+	int j, start, k=0, h=0;
+	for(int i = 0; i < f.nnodes; i++) {
+		if ( f.nodes[i].status == used ) {
+			printf("\n\n------File: %s---------\n", f.nodes[i].path);
+			start = f.nodes[i].start_block;
+			j = 0;
+			while(start!=-1) {
+				printf("blockindex: %d status: %d content: %s\n", start,f.blocks[start].status, f.blocks[start].data);
+				start = f.blocks[start].next_block;	
+				j+=1;
+			}
+			printf("------Blocks in file: %d----------\n",j);
+			k++;
+			h+=j;
+		}
+	}
+	printf("------Total Files: %d----------\n",k);
+	printf("------Total Blocks: %d----------\n",h);
+
+	j = 0;
+	printf("\n\n------Reading Blocks---\n");
+	printf("Blocks: ");
+	for(int i = 0; i < f.nblocks; i++) {
+		if ( f.blocks[i].status == usedblock ) {
+			printf("%d, ", i);
+			j+=1;
+		}
+		else if ( f.blocks[i].status == lastblock ) {
+			printf("Last%d, ", i);
+			j+=1;
+		}
+	}
+	printf("\n------Total Blocks: %d----------\n\n",j);
+	
+
+	return 0;
 }
 
 node* get_free_node()
 {
-	int i = get_free_bit(f.node_bitmap, f.nnodes);
-	f.node_bitmap[i] = 1;
-	return &f.nodes[i];
-}
-
-block* get_free_block()
-{
-	int i = get_free_bit(f.block_bitmap, f.nblocks);
-	f.block_bitmap[i] = 1;
-	return &f.blocks[i];	
-}
-
-int makefile(char *path, char *content)
-{
-	printf("--------------makefile\n");
-	node* file;
-	block* fileblock;
-	block* fileblock2;
-
-	file = get_free_node();
-	
-	file->mode = S_IFREG | 0666;
-	strcpy(file->path, path);
-	file->is_dir = 0;
-	
-	int offset = 0;
-	size_t len = strlen(content);
-
-	fileblock = get_free_block();
-	file->start = fileblock;
-	
-	int size = f.block_size;
-	if (size > len){
-		size = len - offset;
-	}
-	memcpy(fileblock, content, size);
-	offset += size;
-	
-	while ( offset < len ) {
-		if (offset + size > len){
-			size = len - offset;
+	for (int i = 0; i < f.nnodes; i++) {
+		if ( f.nodes[i].status == unused ){
+			f.nodes[i].status = used;
+			return &f.nodes[i];
 		}
-	
-		fileblock2 = get_free_block(f);
-		fileblock->next_block = fileblock2;
-		memcpy(fileblock2, content+offset, size);
-		offset += size;
-		fileblock = fileblock2;
 	}
-
-	fileblock->next_block = NULL;
-	file->size = offset;
-
-	return 0;
+	perror("No more nodes left!!");
+	exit(0);
 }
 
-int makedir(char *path){
-	printf("--------------makedir\n");
-
-	node* file = get_free_node();
-	
-	file->mode = S_IFDIR | 0755;
-	strcpy(file->path, path);
-	file->is_dir = 1;
-	file->start = NULL;
-
-	return 0;
+int get_free_block_index()
+{
+	for (int i = 0; i < f.nblocks; i++) {
+		if ( f.blocks[i].status == unusedblock ){
+			f.blocks[i].status = usedblock;
+			return i;
+		}
+	}
+	perror("No more blocks left!!");
+	exit(0);
 }
 
 void* hello_init(struct fuse_conn_info *conn) 
 {
 	printf("--------------init\n");
-	
 	f.block_size = BLOCK_SIZE;
-	f.nblocks = NBLOCKS;
 	f.nnodes = NNODES;
-
+	f.nblocks = NBLOCKS;
+	
 	f.nodes = (node *) malloc(f.nnodes*sizeof(node));
 	f.blocks = (block *) malloc(f.nblocks*sizeof(block));
-	f.node_bitmap = (int *) malloc(f.nnodes*sizeof(int));
-	f.block_bitmap = (int *) malloc(f.nblocks*sizeof(int));
-
+	
 	printf("--------------ReadingFromDisk\n");
 	fp = fopen(filename, "r");
 	if (fp != NULL) {
 		fread(f.nodes, sizeof(node), f.nnodes, fp);
 		fread(f.blocks, sizeof(block), f.nblocks, fp);
-		fread(f.node_bitmap, sizeof(int), f.nnodes, fp);
-		fread(f.block_bitmap, sizeof(int), f.nblocks, fp);
 	}
 	else {
 		fp = fopen(filename, "w+");
+		nodes_init();
+		blocks_init();
 	}
 	fclose(fp);
-	//makefile("/file.txt","filehellhellomypeople good boy ! poop");
-	//makefile("/b.txt","bahah12323 &8&* !");
-
 	return &f;
 }
 
@@ -148,18 +136,18 @@ void hello_destroy(void *v)
 {
 	printf("--------------WritingToDisk\n");
 	fp = fopen(filename, "w");
-	fwrite(f.nodes, sizeof(node), f.nnodes, fp);
-	fwrite(f.blocks, sizeof(block), f.nblocks, fp);
-	fwrite(f.node_bitmap, sizeof(int), f.nnodes, fp);
-	fwrite(f.block_bitmap, sizeof(int), f.nblocks, fp);
-	fclose(fp);
-
+	if (fp != NULL) {
+		fwrite(f.nodes, sizeof(node), f.nnodes, fp);
+		fwrite(f.blocks, sizeof(block), f.nblocks, fp);
+		fclose(fp);
+	}
+	else {
+		perror("Couldn't open file in write mode");
+	}
+	
 	printf("--------------destroy\n");
 	free(f.nodes);
 	free(f.blocks);
-	free(f.node_bitmap);
-	free(f.block_bitmap);
-
 } 
 
 static int hello_getattr(const char *path, struct stat *stbuf)
@@ -172,8 +160,8 @@ static int hello_getattr(const char *path, struct stat *stbuf)
 		return 0;
 	} 
 	
-	for(int i = 0; i < f.nnodes; i++) {
-		if ( f.node_bitmap[i] == 1 && strcmp(path, f.nodes[i].path) == 0) {
+	for (int i = 0; i < f.nnodes; i++) {
+		if ( f.nodes[i].status == used && strcmp(path, f.nodes[i].path) == 0) {
 			stbuf->st_mode = f.nodes[i].mode;
 			stbuf->st_nlink = 1;
 			stbuf->st_size = f.nodes[i].size;		
@@ -196,21 +184,21 @@ static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 	filler(buf, ".", NULL, 0);
 	filler(buf, "..", NULL, 0);
 
-	for(int i=0; i<f.nnodes; i++) {
-		if ( f.node_bitmap[i]==1 ) {
+	for (int i = 0; i < f.nnodes; i++) {
+		if ( f.nodes[i].status == used ) {
 			filler(buf, f.nodes[i].path+1, NULL, 0);
 		}
 	}
+
+	print_info();
 
 	return 0;
 }
 
 static int hello_open(const char *path, struct fuse_file_info *fi)
 {
-	//if (strcmp(path, hello_path) != 0)
-	//	return -ENOENT;
-	for(int i=0; i<f.nnodes; i++) {
-		if ( f.node_bitmap[i]==1 && strcmp(path, f.nodes[i].path) == 0 ) {
+	for (int i = 0; i < f.nnodes; i++) {
+		if ( f.nodes[i].status == used && strcmp(path, f.nodes[i].path) == 0) {
 			return 0;
 		}
 	}
@@ -228,9 +216,6 @@ static int hello_create(const char *path, mode_t mode, struct fuse_file_info *fi
 		file = get_free_node();
 		file->mode = mode;
 		strcpy(file->path, path);
-		file->is_dir = 0;
-		file->size = 0;
-		file->start = NULL;
 	}
 	
 	return 0;
@@ -243,23 +228,25 @@ static int hello_read(const char *path, char *buf, size_t size, off_t offset,
 	size_t len, offset2=0;
 	(void) fi;
 	
-	block* fileblock;
-	int size2;
+	int fileblock, size2;
 
-	for(int i=0; i<f.nnodes; i++) {
-		if ( f.node_bitmap[i]==1 && strcmp(path, f.nodes[i].path) == 0 ) {
+	for(int i = 0; i < f.nnodes; i++) {
+		if ( f.nodes[i].status == used && strcmp(path, f.nodes[i].path) == 0 ) {
 			len = f.nodes[i].size;
 			size2 = f.block_size;
-			fileblock = f.nodes[i].start;
-
+			fileblock = f.nodes[i].start_block;
+			
+			if (fileblock == -1)
+				return 0;
+			
 			while ( offset2 < len ) {	
-				if ( offset2 + f.block_size > len ){
+				if ( offset2 + size2 > len ){
 					size2 = len - offset2;
 				}
 				
-				memcpy(buf + offset2, fileblock , size2);
-				fileblock = fileblock->next_block;
-				offset2+=size2;
+				memcpy(buf + offset2, &f.blocks[fileblock].data , size2);
+				fileblock = f.blocks[fileblock].next_block;
+				offset2 += size2;
 			}			
 			return offset2;
 		}
@@ -271,8 +258,43 @@ static int hello_write(const char *path, const char *buf, size_t size, off_t off
 		      struct fuse_file_info *fi)
 {
 	//return number of bytes written
-	return 0;
+	size_t offset2 = 0;
+	int fileblock, last, size2;
+	
+	for(int i=0; i<f.nnodes; i++) {
+		if ( f.nodes[i].status == used && strcmp(path, f.nodes[i].path) == 0 ) {
+			size2 = f.block_size;
+			fileblock = f.nodes[i].start_block;
 
+			if (fileblock == -1) {
+				fileblock = get_free_block_index();
+				f.nodes[i].start_block = fileblock;
+			}
+
+			while ( offset2 < size ) {	
+				if ( offset2 + size2 > size ) {
+					size2 = size - offset2;
+				}
+				
+				if ( fileblock == -1 ) {
+					fileblock = get_free_block_index();
+					f.blocks[last].next_block = fileblock;
+				}
+
+				memcpy(&f.blocks[fileblock].data, buf + offset2 , size2);
+				
+				last = fileblock;
+				fileblock = f.blocks[fileblock].next_block;
+				offset2 += size2;
+			}
+
+			f.blocks[last].status = lastblock;
+			f.nodes[i].size = offset2;
+			
+			return offset2;	 
+		}
+	}
+	return -ENOENT;
 }
 
 static struct fuse_operations hello_oper = {
--- rdstructs.h
+++ rdstructs.h
@@ -3,26 +3,31 @@
 #define NBLOCKS 200
 #define NNODES 200
 
-typedef struct b
+typedef enum {usedblock, lastblock, unusedblock} block_status;
+typedef enum {used, unused} node_status;
+
+typedef struct
 {
+	block_status status;// = unusedblock;
 	char data[BLOCK_SIZE];
-	struct b* next_block;
+	int next_block;// = -1;
 } block;
 
 typedef struct 
 {
+	node_status status;// = unused;
 	char path[PATH_MAX];
-	int is_dir;
+	int is_dir;// = 0;
 	mode_t mode;
-	size_t size;
-	block* start;
+	size_t size;// = 0;
+	int start_block;// = -1;
 } node;
 
 typedef struct 
 {
-	size_t block_size;
-	size_t nblocks;
-	size_t nnodes;
+	size_t block_size;// = BLOCK_SIZE;
+	size_t nblocks;// = NBLOCKS;
+	size_t nnodes;// = NNODES;
 	node* nodes;
 	block* blocks;
 	int* block_bitmap;
